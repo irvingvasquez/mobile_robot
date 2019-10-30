7a5b494f4bfb932ffda8e0b6780cf8741f3a183f
Irving Vasquez <ivasquez@ccc.inaoep.mx>
2019-10-30 16:09:42 -0600

    add solution

--- utils.py
+++ utils.py
@@ -7,8 +7,9 @@ import numpy as np
 def plotPolygons(polygons):
     fig, ax = plt.subplots(1)
     for element in polygons:
-        x, y = element.exterior.coords.xy
-        points = np.array([x, y], np.int32).T
+        points = element.exterior.coords.xy
+        
+        points = np.transpose(points)
         polygon_shape = matplotlib.patches.Polygon(points, linewidth=1, edgecolor='r', facecolor='none')
         ax.add_patch(polygon_shape)
         plt.axis("equal")
